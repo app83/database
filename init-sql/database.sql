0871dbaf6fefc4b53076bc3cf8d9b3c8f810b526
app83 <45634002+app83@users.noreply.github.com>
2020-05-11 14:43:39 -0400

    Update database.sql

--- init-sql/database.sql
+++ init-sql/database.sql
@@ -1,41 +1,23 @@
-CREATE DATABASE sis;
-USE sis;
-CREATE TABLE student_info (
-    id INTEGER,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    PRIMARY KEY (id)
-);
-CREATE TABLE student_contacts (
+CREATE DATABASE meds;
+USE meds;
+CREATE TABLE med_info (
     id INTEGER,
     name VARCHAR(255),
-    phone VARCHAR(10),
+    abbrev VARCHAR(255),
+    quantity INTEGER,
     PRIMARY KEY (id)
 );
-CREATE TABLE student_attendance (
-    object_id INTEGER NOT NULL AUTO_INCREMENT,
-    student_id INT,
-    code CHAR,
-    credit FLOAT,
-    PRIMARY KEY (object_id)
-);
-
-INSERT INTO student_info (id, first_name, last_name) 
+INSERT INTO med_info (id, name, abbrev, quantity) 
 VALUES
-    (0, "Abe", "Johnson"),
-    (1, "Om", "Patel"),
-    (2, "Kie", "Nguyen"),
-    (3, "Curtis", "Mayfield"),
-    (4, "Emily", "Gao");
+    (0, "Acetaminophen", "Tylenol", 200),
+    (1, "Ibuprofen", "Advil", 250),
+    (2, "Tenormin", "Atenolol", 100),
+    (3, "Folate", "Folic Acid", 50),
+    (4, "Naproxen", "Aleve", 150);
 
-INSERT INTO student_contacts (id, name, phone) 
-VALUES
-    (0, "Frank Johnson", "7235555555"),
-    (2, "Parik Nguyen", "6095555555"),
-    (3, "Olga Preel", "2125555555");
-
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'it635';
+/*ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 CREATE USER 'steve'@'%' IDENTIFIED BY 'it635';
 GRANT SELECT ON sis.student_contacts TO 'steve'@'%';
 GRANT INSERT ON sis.student_attendance TO 'steve'@'%';
 FLUSH PRIVILEGES;
+*/
